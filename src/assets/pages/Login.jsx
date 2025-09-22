bcad25f25bbf2c7e80d445400fa28b7193b41688
JunYong <chary8028@naver.com>
2025-09-22 21:55:09 +0900

    Feat: KAN-31 project setting

---
+++ src/assets/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <span>test</span>
+    </div>
+  );
+};
+
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "@/assets/css/font.css";
+import "@/assets/css/global.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  // StrickMode 임시 주석 처리
+  <>
     <App />
-  </StrictMode>,
-)
+  </>,
+);
