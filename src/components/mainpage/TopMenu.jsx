ca14a8831d443dfeeb49d1473c4c21ed387452fc
JunYong <chary8028@naver.com>
2025-09-26 12:10:57 +0900

    Feat: KAN-40 포인트샵 페이지 레이아웃 구현

--- src/components/mainpage/TopMenu.jsx
+++ src/components/mainpage/TopMenu.jsx
@@ -1,34 +1,48 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { CiShop } from "react-icons/ci";
 import { FaStar } from "react-icons/fa";
 import { IoCalendarNumberOutline } from "react-icons/io5";
 import { FaHospitalUser } from "react-icons/fa6";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function TopMenu() {
+  const navigate = useNavigate();
+  const location = useLocation(); // 현재 URL 경로 정보를 가져온다.
+
   const menus = [
-    { name: "포인트샵", icon: <CiShop size={32} /> },
-    { name: "포인트 충전", icon: <FaStar size={32} /> },
-    { name: "상품 쇼핑", icon: <IoCalendarNumberOutline size={32} /> },
-    { name: "마이 페이지", icon: <FaHospitalUser size={32} /> },
+    { name: "포인트샵", icon: <CiShop size={32} />, path: "/pointshop" },
+    { name: "포인트 현황", icon: <FaStar size={32} />, path: "/pointhisory" },
+    {
+      name: "일정 관리",
+      icon: <IoCalendarNumberOutline size={32} />,
+      path: "/schedule",
+    },
+    {
+      name: "건강 관리",
+      icon: <FaHospitalUser size={32} />,
+      path: "/healthcheck",
+    },
   ];
 
-  const [activeMenuName, setActiveMenuName] = useState("");
-
-  const handleClick = (menuName) => {
-    setActiveMenuName(menuName);
+  const handleClick = (path) => {
+    // 메뉴 클릭 시 해당 경로로 이동만.
+    navigate(path);
   };
 
   return (
     <MenuContainer>
       {menus.map((menu) => {
-        const isActive = menu.name === activeMenuName;
+        // 현재 URL 경로($`location.pathname`$)와 메뉴의 경로($`menu.path`$)를 비교하여 활성 상태를 결정.
+        const isActive = location.pathname === menu.path;
 
         return (
           <MenuItem
             key={menu.name}
+            // activeMenuName 대신 isActive를 props로 전달.
             $active={isActive}
-            onClick={() => handleClick(menu.name)}
+            // 메뉴의 name 대신 path를 handleClick에 전달.
+            onClick={() => handleClick(menu.path)}
           >
             <IconPlaceholder $active={isActive}>{menu.icon}</IconPlaceholder>
             {menu.name}
